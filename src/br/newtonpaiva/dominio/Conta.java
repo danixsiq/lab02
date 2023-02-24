054b9bd2143e9f7651e7f1f762d4aed150d4578c
12117309 <12117309>
2023-02-24 20:26:27 -0300

    feat: cria a classe conta

---
+++ src/br/newtonpaiva/dominio/Conta.java
@@ -0,0 +1,20 @@
+package br.newtonpaiva.dominio;
+
+public class Conta {
+    private Integer numero;
+    private Double saldo;
+
+    public Double sacar(Double valor){
+        return 0.0;
+    }
+    public Double depositar (Double valor){
+        return 0.0;
+    }
+    public Integer getNumero (){
+        return numero;
+    }
+    public void setNumero (Integer numero){
+        this.numero= numero;
+    }
+
+}
